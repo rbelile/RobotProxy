19619cd329147c9f77ab4b33b96b9cd80afb58fa
Gmeaders <gmeader@g.clemson.edu>
2015-11-27 20:16:55 -0500

    Udp basic echo client and server
    Added a basic echo client to robotClient and server to robotProxy,
    currently doesn't take names as server address Ex. needs 130.127.201.206
    instead of localhost

--- robotClient.cpp
+++ robotClient.cpp
@@ -4,12 +4,89 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
+#define MAXSTRINGLENGTH 300
 using namespace std;
+
+void DieWithError(string errorMessage){ // Error handling function
+    fprintf(stdout, "%s\n", errorMessage.c_str());
+    exit(1);
+}
+
+
+void UdpClient(string server, char* servPort){
+
+  	
+  	string echoString;
+  	cout << "Please enter an echo message: " << endl;
+  	getline(cin, echoString);
+
+  	size_t echoStringLen = echoString.length();
+  	if (echoStringLen > 300) // Check input length
+    	DieWithError("string too long");
+
+  	// Tell the system what kind(s) of address info we want
+  	struct addrinfo addrCriteria;                   // Criteria for address match
+  	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
+  	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
+  	addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
+  	addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
+
+  	// Get address(es)
+  	struct addrinfo *servAddr; // List of server addresses
+  	int rtnVal = getaddrinfo(server.c_str(), servPort, &addrCriteria, &servAddr);
+  	if (rtnVal != 0)
+    	DieWithError("getaddrinfo() failed");
+
+  	// Create a datagram/UDP socket
+  	int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
+      	servAddr->ai_protocol); // Socket descriptor for client
+  	if (sock < 0)
+    	DieWithError("socket() failed");
+
+  	// Send the string to the server
+  	ssize_t numBytes = sendto(sock, echoString.c_str(), echoStringLen, 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  	if (numBytes < 0)
+    	DieWithError("sendto() failed");
+  	else if (numBytes != echoStringLen)
+    	DieWithError("sendto() error, sent unexpected number of bytes");
+
+  	// Receive a response
+
+  	struct sockaddr_storage fromAddr; // Source address of server
+  	// Set length of from address structure (in-out parameter)
+  	socklen_t fromAddrLen = sizeof(fromAddr);
+
+  	char buffer[MAXSTRINGLENGTH + 1]; // I/O buffer
+
+  	numBytes = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+
+  	if (numBytes < 0)
+    	DieWithError("recvfrom() failed");
+  	else if (numBytes != echoStringLen)
+    	DieWithError("recvfrom() error, received unexpected number of bytes");
+
+  	freeaddrinfo(servAddr);
+
+  	buffer[echoStringLen] = '\0';     // Null-terminate received data
+  	printf("Received: %s\n", buffer); // Print the echoed string
+
+  	close(sock);
+  	exit(0);
+}
+
 
 int main(int argc, char *argv[]) {
 	string serverName;
 	in_port_t serverPort;
+	char *UdpClientPort;
 	int lenOfSides, numOfSides;
 
 	if (argc != 9) {
@@ -28,7 +105,9 @@ int main(int argc, char *argv[]) {
 			numOfSides = atoi(argv[i+1]);
 		} else if (argvStr.compare("-p") == 0) {
 			serverPort = atoi(argv[i+1]);
+			UdpClientPort = argv[i+1];
 		}
 	}
 
+	UdpClient(serverName, UdpClientPort);
 }
--- robotProxy.cpp
+++ robotProxy.cpp
@@ -4,13 +4,77 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
 
+#define MAXSTRINGLENGTH 300
 using namespace std;
+
+void DieWithError(string errorMessage){ // Error handling function
+    fprintf(stdout, "%s\n", errorMessage.c_str());
+    exit(1);
+}
+
+void UdpServer(char *port){
+
+	
+	// Construct the server address structure
+  	struct addrinfo addrCriteria;                   // Criteria for address
+  	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
+  	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
+  	addrCriteria.ai_flags = AI_PASSIVE;             // Accept on any address/port
+  	addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram socket
+  	addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP socket
+
+  	struct addrinfo *servAddr; // List of server addresses
+  	int rtnVal = getaddrinfo(NULL, port, &addrCriteria, &servAddr);
+  	if (rtnVal != 0)
+    	DieWithError("getaddrinfo() failed");
+
+  	// Create socket for incoming connections
+  	int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
+      	servAddr->ai_protocol);
+  	if (sock < 0)
+    	DieWithError("socket() failed");
+
+  	// Bind to the local address
+  	if (bind(sock, servAddr->ai_addr, servAddr->ai_addrlen) < 0)
+    	DieWithError("bind() failed");
+
+  	// Free address list allocated by getaddrinfo()
+  	freeaddrinfo(servAddr);
+  	for (;;) { // Run forever
+    	struct sockaddr_storage clntAddr; // Client address
+    	// Set Length of client address structure (in-out parameter)
+    	socklen_t clntAddrLen = sizeof(clntAddr);
+
+    	// Block until receive message from a client
+    	 char buffer[MAXSTRINGLENGTH]; // I/O buffer
+    	// Size of received message
+    	ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+        	(struct sockaddr *) &clntAddr, &clntAddrLen);
+    	if (numBytesRcvd < 0)
+      		DieWithError("recvfrom() failed");
+
+    	// Send received datagram back to the client
+    	ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
+        	(struct sockaddr *) &clntAddr, sizeof(clntAddr));
+
+    	if (numBytesSent < 0)
+      		DieWithError("sendto() failed)");
+    	else if (numBytesSent != numBytesRcvd)
+      		DieWithError("sendto() sent unexpected number of bytes");
+  }
+
+}
 
 int main(int argc, char *argv[]) {
 	string hostName;
 	int robotID, robotNum;
 	in_port_t port;
+	char* UdpServerPort;
 
 	if (argc != 9) {
 		cout << "Usage: " << argv[0] << " -h <hostname-of-robot> -i <robot-id> -n " <<
@@ -28,6 +92,9 @@ int main(int argc, char *argv[]) {
 			robotNum = atoi(argv[i+1]);
 		} else if (argvStr.compare("-p") == 0) {
 			port = atoi(argv[i+1]);
+			UdpServerPort = argv[i+1];
 		}
 	}
+
+	UdpServer(UdpServerPort);
 }
