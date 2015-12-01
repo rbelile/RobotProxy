3833145653df89f4551293bf271b5d7c0d32433f
Daniel Battle <dbattle@g.clemson.edu>
2015-11-30 21:04:12 -0500

    Current client and proxy

--- robotClient.cpp
+++ robotClient.cpp
@@ -9,8 +9,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sstream>
+#include <iomanip>
 
-#define MAXSTRINGLENGTH 300
+#define MAXDATAGRAM 300
+#define MAXGROUPDATAGRAM 600
 using namespace std;
 
 void DieWithError(string errorMessage){ // Error handling function
@@ -18,66 +21,21 @@ void DieWithError(string errorMessage){ // Error handling function
     exit(1);
 }
 
+string getDate() {
+    time_t t = time(0);   // get time now
+    struct tm * now = localtime( & t );
+    int year = now->tm_year + 1900;
+    int month = now->tm_mon + 1;
+    int day = now->tm_mday;
+    int hour = now->tm_hour;
+    int min = now->tm_min; 
+    int sec = now->tm_sec;
 
-void UdpClient(string server, char* servPort){
-  	string echoString;
-  	//cout << "Please enter an echo message: " << endl;
-  	//getline(cin, echoString);
+    stringstream ss;
 
-  	size_t echoStringLen = echoString.length();
-  	if (echoStringLen > 300) // Check input length
-    	DieWithError("string too long");
-
-  	// Tell the system what kind(s) of address info we want
-  	struct addrinfo addrCriteria;                   // Criteria for address match
-  	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-  	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
-  	addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
-  	addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
-
-  	// Get address(es)
-  	struct addrinfo *servAddr; // List of server addresses
-  	int rtnVal = getaddrinfo(server.c_str(), servPort, &addrCriteria, &servAddr);
-  	if (rtnVal != 0)
-    	DieWithError("getaddrinfo() failed");
-
-  	// Create a datagram/UDP socket
-  	int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
-      	servAddr->ai_protocol); // Socket descriptor for client
-  	if (sock < 0)
-    	DieWithError("socket() failed");
-
-  	// Send the string to the server
-  	ssize_t numBytes = sendto(sock, echoString.c_str(), echoStringLen, 0,
-      	servAddr->ai_addr, servAddr->ai_addrlen);
-  	if (numBytes < 0)
-    	DieWithError("sendto() failed");
-  	else if (numBytes != echoStringLen)
-    	DieWithError("sendto() error, sent unexpected number of bytes");
-
-  	// Receive a response
-
-  	struct sockaddr_storage fromAddr; // Source address of server
-  	// Set length of from address structure (in-out parameter)
-  	socklen_t fromAddrLen = sizeof(fromAddr);
-
-  	char buffer[MAXSTRINGLENGTH + 1]; // I/O buffer
-
-  	numBytes = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
-      	(struct sockaddr *) &fromAddr, &fromAddrLen);
-
-  	if (numBytes < 0)
-    	DieWithError("recvfrom() failed");
-  	else if (numBytes != echoStringLen)
-    	DieWithError("recvfrom() error, received unexpected number of bytes");
-
-  	freeaddrinfo(servAddr);
-
-  	buffer[echoStringLen] = '\0';     // Null-terminate received data
-  	printf("Received: %s\n", buffer); // Print the echoed string
-
-  	close(sock);
-  	exit(0);
+    ss << "" << month << "/" << day << "/" << year << " " << hour << ":"
+        << setfill('0') << setw(2) << min << ":" << setfill('0') << setw(2) << sec;
+    return ss.str();
 }
 
 int main(int argc, char *argv[]) {
@@ -106,5 +64,397 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	UdpClient(serverName, UdpClientPort);
+	int udpHeaders[7] = { 0, 0, 0, 0, 1, 0, 0 }; // To connect to proxy
+	/*cout << "Headers: ";
+	for(int i = 0; i < 7; i++) {
+		cout << udpHeaders[i] << " ";
+	}
+	cout << endl;*/
+
+  	// Tell the system what kind(s) of address info we want
+  	struct addrinfo addrCriteria;                   // Criteria for address match
+  	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
+  	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
+  	addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram sockets
+  	addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP protocol
+
+  	// Get address(es)
+  	struct addrinfo *servAddr; // List of server addresses
+  	int rtnVal = getaddrinfo(serverName.c_str(), UdpClientPort, &addrCriteria, &servAddr);
+  	if (rtnVal != 0)
+    	DieWithError("getaddrinfo() failed");
+
+  	// Create a datagram/UDP socket
+  	int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
+      	servAddr->ai_protocol); // Socket descriptor for client
+  	if (sock < 0)
+    	DieWithError("socket() failed");
+
+  	// Send the string to the server
+  	ssize_t numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  	if (numBytes < 0)
+    	DieWithError("sendto() failed");
+  	else if (numBytes != sizeof(udpHeaders))
+    	DieWithError("sendto() error, sent unexpected number of bytes");
+
+  	// Receive a response
+
+  	struct sockaddr_storage fromAddr; // Source address of server
+  	// Set length of from address structure (in-out parameter)
+  	socklen_t fromAddrLen = sizeof(fromAddr);
+
+  	int buffer[7]; // I/O buffer
+
+  	ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+
+  	if (numBytesRcvd < 0)
+    	DieWithError("recvfrom() failed");
+
+	int password = buffer[1];
+
+	udpHeaders[1] = password;
+
+  	numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  	if (numBytes < 0)
+    	DieWithError("sendto() failed");
+  	else if (numBytes != sizeof(udpHeaders))
+    	DieWithError("sendto() error, sent unexpected number of bytes");
+
+	bool isOnSecond = false;
+
+repeat:
+	for(int i = 0; i < numOfSides; i++) {
+		// Image
+		udpHeaders[2] = 2;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 2) {
+			DieWithError("Server did not acknowledge image request.");
+		}
+
+		ofstream imageFile;
+		string fileName = getDate() + " Camera Data.jpeg";
+		imageFile.open(fileName.c_str());
+
+		char fullBuffer[MAXDATAGRAM];
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		string tempStr = "" + fullBuffer[20] + fullBuffer[21] + fullBuffer[22] 
+			+ fullBuffer[23];
+		int numOfDatagrams = atoi(tempStr.c_str());
+		for(int j = 28; j < 300; j++) {
+			imageFile << fullBuffer[j];
+		}
+
+		for(int j = 2; j <= numOfDatagrams; j++) {
+			numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+		   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+			for(int j = 28; j < 300; j++) {
+				imageFile << fullBuffer[j];
+			}
+		}
+		imageFile.close();
+
+		// GPS
+		udpHeaders[2] = 4;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 4) {
+			DieWithError("Server did not acknowledge image request.");
+		}
+
+		ofstream gpsFile;
+		fileName = getDate() + " GPS Data.jpeg";
+		gpsFile.open(fileName.c_str());
+		
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		for(int j = 28; j < 300; j++) {
+			gpsFile << fullBuffer[j];
+		}
+		gpsFile.close();
+
+		// dGPS
+		udpHeaders[2] = 8;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 8) {
+			DieWithError("Server did not acknowledge image request.");
+		}
+
+		ofstream dgpsFile;
+		fileName = getDate() + " dGPS Data.jpeg";
+		dgpsFile.open(fileName.c_str());
+		
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		for(int j = 28; j < 300; j++) {
+			dgpsFile << fullBuffer[j];
+		}
+		dgpsFile.close();
+
+		// Laser
+		udpHeaders[2] = 16;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 16) {
+			DieWithError("Server did not acknowledge image request.");
+		}
+
+		ofstream laserFile;
+		fileName = getDate() + " Laser Data.jpeg";
+		laserFile.open(fileName.c_str());
+
+		// reset fullbuffer?
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		tempStr = "" + fullBuffer[20] + fullBuffer[21] + fullBuffer[22] 
+			+ fullBuffer[23];
+		numOfDatagrams = atoi(tempStr.c_str());
+		for(int j = 28; j < 300; j++) {
+			laserFile << fullBuffer[j];
+		}
+
+		for(int j = 2; j <= numOfDatagrams; j++) {
+			numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+		   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+			for(int j = 28; j < 300; j++) {
+				laserFile << fullBuffer[j];
+			}
+		}
+		laserFile.close();
+
+		// Move
+		udpHeaders[2] = 32;
+		udpHeaders[3] = lenOfSides;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 32) {
+			DieWithError("Server did not acknowledge move request.");
+		}
+
+		// Stop
+		udpHeaders[2] = 128;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 128) {
+			DieWithError("Server did not acknowledge stop request.");
+		}
+
+		// Turn
+		int angle = 180 * (numOfSides - 2) / numOfSides;
+		int speed = 1.4 * angle / 360;
+		udpHeaders[2] = 64;
+		udpHeaders[3] = speed;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 64) {
+			DieWithError("Server did not acknowledge turn request.");
+		}
+
+		// Stop
+		udpHeaders[2] = 128;
+		numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+      	servAddr->ai_addr, servAddr->ai_addrlen);
+  		if (numBytes < 0)
+    		DieWithError("sendto() failed");
+  		else if (numBytes != sizeof(udpHeaders))
+    		DieWithError("sendto() error, sent unexpected number of bytes");
+
+		numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+      	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		if (buffer[2] != 128) {
+			DieWithError("Server did not acknowledge stop request.");
+		}
+	}
+	// Image
+	udpHeaders[2] = 2;
+	numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+   	servAddr->ai_addr, servAddr->ai_addrlen);
+	if (numBytes < 0)
+ 		DieWithError("sendto() failed");
+	else if (numBytes != sizeof(udpHeaders))
+ 		DieWithError("sendto() error, sent unexpected number of bytes");
+
+	numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	if (buffer[2] != 2) {
+		DieWithError("Server did not acknowledge image request.");
+	}
+
+	ofstream imageFile;
+	string fileName = getDate() + " Camera Data.jpeg";
+	imageFile.open(fileName.c_str());
+
+	char fullBuffer[MAXDATAGRAM];
+	numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	string tempStr = "" + fullBuffer[20] + fullBuffer[21] + fullBuffer[22] 
+		+ fullBuffer[23];
+	int numOfDatagrams = atoi(tempStr.c_str());
+	for(int j = 28; j < 300; j++) {
+		imageFile << fullBuffer[j];
+	}
+
+	for(int j = 2; j <= numOfDatagrams; j++) {
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+	   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		for(int j = 28; j < 300; j++) {
+			imageFile << fullBuffer[j];
+		}
+	}
+	imageFile.close();
+
+	// GPS
+	udpHeaders[2] = 4;
+	numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+   	servAddr->ai_addr, servAddr->ai_addrlen);
+	if (numBytes < 0)
+ 		DieWithError("sendto() failed");
+	else if (numBytes != sizeof(udpHeaders))
+ 		DieWithError("sendto() error, sent unexpected number of bytes");
+
+	numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	if (buffer[2] != 4) {
+		DieWithError("Server did not acknowledge image request.");
+	}
+
+	ofstream gpsFile;
+	fileName = getDate() + " GPS Data.jpeg";
+	gpsFile.open(fileName.c_str());
+	
+	numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	for(int j = 28; j < 300; j++) {
+		gpsFile << fullBuffer[j];
+	}
+	gpsFile.close();
+
+	// dGPS
+	udpHeaders[2] = 8;
+	numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+   	servAddr->ai_addr, servAddr->ai_addrlen);
+	if (numBytes < 0)
+ 		DieWithError("sendto() failed");
+	else if (numBytes != sizeof(udpHeaders))
+ 		DieWithError("sendto() error, sent unexpected number of bytes");
+
+	numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	if (buffer[2] != 8) {
+		DieWithError("Server did not acknowledge image request.");
+	}
+
+	ofstream dgpsFile;
+	fileName = getDate() + " dGPS Data.jpeg";
+	dgpsFile.open(fileName.c_str());
+	
+	numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	for(int j = 28; j < 300; j++) {
+		dgpsFile << fullBuffer[j];
+	}
+	dgpsFile.close();
+
+	// Laser
+	udpHeaders[2] = 16;
+	numBytes = sendto(sock, udpHeaders, sizeof(udpHeaders), 0,
+   	servAddr->ai_addr, servAddr->ai_addrlen);
+	if (numBytes < 0)
+ 		DieWithError("sendto() failed");
+	else if (numBytes != sizeof(udpHeaders))
+ 		DieWithError("sendto() error, sent unexpected number of bytes");
+
+	numBytesRcvd = recvfrom(sock, buffer, MAXDATAGRAM, 0,
+   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+	if (buffer[2] != 16) {
+		DieWithError("Server did not acknowledge image request.");
+	}
+
+	ofstream laserFile;
+	fileName = getDate() + " Laser Data.jpeg";
+	laserFile.open(fileName.c_str());
+
+	//char fullBuffer[MAXDATAGRAM];
+	numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+   		(struct sockaddr *) &fromAddr, &fromAddrLen);
+	tempStr = "" + fullBuffer[20] + fullBuffer[21] + fullBuffer[22] 
+		+ fullBuffer[23];
+	numOfDatagrams = atoi(tempStr.c_str());
+	for(int j = 28; j < 300; j++) {
+		laserFile << fullBuffer[j];
+	}
+
+	for(int j = 2; j <= numOfDatagrams; j++) {
+		numBytesRcvd = recvfrom(sock, fullBuffer, MAXDATAGRAM, 0,
+	   	(struct sockaddr *) &fromAddr, &fromAddrLen);
+		for(int j = 28; j < 300; j++) {
+			laserFile << fullBuffer[j];
+		}
+	}
+	laserFile.close();
+
+	if (!isOnSecond) {
+		isOnSecond = true;
+		numOfSides = numOfSides - 1;
+		goto repeat;
+	}
+
+	freeaddrinfo(servAddr);
+
+  	close(sock);
+  	exit(0);
 }
--- robotProxy.cpp
+++ robotProxy.cpp
@@ -8,68 +8,184 @@
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <time.h>
 
 #define MAXSTRINGLENGTH 300
 using namespace std;
+
+int generatePassword();
+string getcommand(int check);
+
+ string hostName;
+ int robotID, robotNum;
+ in_port_t port;
+ char* UdpServerPort;
 
 void DieWithError(string errorMessage){ // Error handling function
     fprintf(stdout, "%s\n", errorMessage.c_str());
     exit(1);
 }
 
+string getCommand(int check){
+
+    if(check == 0)
+    {
+      return "Connect";
+    }
+    else if(Check == 2){
+      return "Image"
+    }
+    else if(Check == 4){
+      return "GPS";
+    }
+    else if(Check == 8){
+      return "dGPS";
+    }
+    else if(Check == 16){
+      return "Lasers"; 
+    }
+    else if(Check == 32){
+      return "Move";
+    }
+    else if(Check == 64){
+      return "Turn";
+    }
+    else if(Check == 128){
+      return "Stop";
+    }
+    else if(Check == 255){
+      return "Quit";
+    }
+    else if(Check == 256){
+      return "Error with Client Request";
+    }
+    else if(Check == 512){
+      return "Error from Robot";
+    }
+    else return "Command not recognized";
+}
+
+string checkCommand(string command){
+  if(command == "Image") return "8081";
+  else if(command == "GPS") return "8082";
+  else if(command == "dGPS") return "8084";
+  else if(command == "Lasers") return "8083";
+  else if(command == "Move") return "8082";
+  else if(command == "Turn") return "8082";
+  else if(command == "Stop") return "8082";
+  else if(command == "Error From Client Request") return "Bad1";
+  else if(command == "Error from Robot") return "Bad2";
+  else return "Bad";
+}
+
 void UdpServer(char *port){
-	// Construct the server address structure
-  	struct addrinfo addrCriteria;                   // Criteria for address
-  	memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
-  	addrCriteria.ai_family = AF_UNSPEC;             // Any address family
-  	addrCriteria.ai_flags = AI_PASSIVE;             // Accept on any address/port
-  	addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram socket
-  	addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP socket
+  // Construct the server address structure
+    struct addrinfo addrCriteria;                   // Criteria for address
+    memset(&addrCriteria, 0, sizeof(addrCriteria)); // Zero out structure
+    addrCriteria.ai_family = AF_UNSPEC;             // Any address family
+    addrCriteria.ai_flags = AI_PASSIVE;             // Accept on any address/port
+    addrCriteria.ai_socktype = SOCK_DGRAM;          // Only datagram socket
+    addrCriteria.ai_protocol = IPPROTO_UDP;         // Only UDP socket
 
-  	struct addrinfo *servAddr; // List of server addresses
-  	int rtnVal = getaddrinfo(NULL, port, &addrCriteria, &servAddr);
-  	if (rtnVal != 0)
-    	DieWithError("getaddrinfo() failed");
+    struct addrinfo *servAddr; // List of server addresses
+    int rtnVal = getaddrinfo(NULL, port, &addrCriteria, &servAddr);
+    if (rtnVal != 0)
+      DieWithError("getaddrinfo() failed");
 
-  	// Create socket for incoming connections
-  	int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
-      	servAddr->ai_protocol);
-  	if (sock < 0)
-    	DieWithError("socket() failed");
+    // Create socket for incoming connections
+    int sock = socket(servAddr->ai_family, servAddr->ai_socktype,
+        servAddr->ai_protocol);
+    if (sock < 0)
+      DieWithError("socket() failed");
 
-  	// Bind to the local address
-  	if (bind(sock, servAddr->ai_addr, servAddr->ai_addrlen) < 0)
-    	DieWithError("bind() failed");
+    // Bind to the local address
+    if (bind(sock, servAddr->ai_addr, servAddr->ai_addrlen) < 0)
+      DieWithError("bind() failed");
 
-  	// Free address list allocated by getaddrinfo()
-  	freeaddrinfo(servAddr);
-  	for (;;) { // Run forever
-    	struct sockaddr_storage clntAddr; // Client address
-    	// Set Length of client address structure (in-out parameter)
-    	socklen_t clntAddrLen = sizeof(clntAddr);
+    // Free address list allocated by getaddrinfo()
+    freeaddrinfo(servAddr);
+    for (;;) { // Run forever
+      struct sockaddr_storage clntAddr; // Client address
 
-    	// Block until receive message from a client
-    	char buffer[MAXSTRINGLENGTH]; // I/O buffer
-    	// Size of received message
-    	ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
-        	(struct sockaddr *) &clntAddr, &clntAddrLen);
-    	if (numBytesRcvd < 0)
-      		DieWithError("recvfrom() failed");
+      // Set Length of client address structure (in-out parameter)
+      socklen_t clntAddrLen = sizeof(clntAddr);
 
-        cout << "here";
+      // Block until receive message from a client
+      int buffer[7]; // I/O buffer
 
-    	// Send received datagram back to the client
-    	ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0,
-        	(struct sockaddr *) &clntAddr, sizeof(clntAddr));
+      // Size of received message
+      ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+          (struct sockaddr *) &clntAddr, &clntAddrLen);
+      if (numBytesRcvd < 0)
+          DieWithError("recvfrom() failed");
 
-    	if (numBytesSent < 0)
-      		DieWithError("sendto() failed");
-    	else if (numBytesSent != numBytesRcvd)
-      		DieWithError("sendto() sent unexpected number of bytes");
+      buffer[1] = generatePassword();
+      int password = buffer[1];
+
+      //cout << "Password: " << buffer[1] << endl;
+
+        // Send received datagram back to the client
+        ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0, 
+        (struct sockaddr *) &clntAddr, sizeof(clntAddr));
+
+        if (numBytesSent < 0)
+            DieWithError("sendto() failed");
+
+      for (;;) {
+
+          ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+            (struct sockaddr *) &clntAddr, &clntAddrLen);
+          if (numBytesRcvd < 0)
+              DieWithError("recvfrom() failed");
+          if(buffer[1] != password){
+            break;
+          }
+        
+          string command = getCommand(buffer[2]);
+          int requestData = buffer[3]; // Value of the move and turn commands.
+          //int sequenceNumber = buffer[4];
+          if(command == "Command not recognized") break;
+          if(command == "255") break;
+
+          string payload = TcpClient(hostName, robotID, robotNum, command, requestData);
+          if(payload == "Error0") break;
+          else if(payload == "Error1"){
+            cout << "Error with Client Request, waiting for new connection" << endl;
+            break;
+          } 
+          else if(payload == "Error2"){
+            cout << "Error from Robot, waiting for new connection" << endl;
+            break;
+          }
+
+      
+      }
+      
+      ssize_t numBytesRcvd = recvfrom(sock, buffer, MAXSTRINGLENGTH, 0,
+          (struct sockaddr *) &clntAddr, &clntAddrLen);
+      if (numBytesRcvd < 0)
+          DieWithError("recvfrom() failed");
+      
+      ssize_t numBytesSent = sendto(sock, buffer, numBytesRcvd, 0, 
+      (struct sockaddr *) &clntAddr, sizeof(clntAddr));
+
+	  close(sock);
+        //else if (numBytesSent != numBytesRcvd)
+          //  DieWithError("sendto() sent unexpected number of bytes");
     }
 }
 
-void TcpClient(string Server, int robotID, int robotNum, char* port){
+string TcpClient(string Server, int robotID, int robotNum, string command, int requestData){
+
+    char* port;
+
+    if(checkCommand(command) == "Bad") return "Error0";
+    else if(checkCommand == "Bad1") return "Error1";
+    else if(checkCommand == "Bad2") return "Error2";
+    else port = checkCommand(command).c_str();
+    
+    string payload;
     int sock;                        /* Socket descriptor */
     struct sockaddr_in ServAddr; /*  server address */
     string Buffer;       
@@ -85,8 +201,8 @@ void TcpClient(string Server, int robotID, int robotNum, char* port){
     /* Construct the server address structure */
     memset(&ServAddr, 0, sizeof(ServAddr));     /* Zero out structure */
     ServAddr.sin_family      = AF_INET;             /* Internet address family */
-    ServAddr.sin_addr.s_addr = inet_addr(Server.c_str();   /* Server IP address */
-    ServAddr.sin_port        = htons(ServPort); /* Server port */
+    ServAddr.sin_addr.s_addr = inet_addr(Server.c_str());   // Server IP address
+    //ServAddr.sin_port        = htons(port); /* Server port */
 
 
     if(ServAddr.sin_addr.s_addr == -1)
@@ -98,36 +214,65 @@ void TcpClient(string Server, int robotID, int robotNum, char* port){
     /* Establish the connection to the  server */
     if (connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) < 0)
         DieWithError("connect() failed");
+        
+    string HTTPreq = "http://" + hostname + ":";    
+	switch(command){
+		case 2:
+			HTTPreq += "8081/snapshot?topic=/robot9/image?width=600?height=500";
+			break;
+		case 4:
+			HTTPreq += "8082/state?id=5senior";
+			break;
+		case 8:
+			HTTPreq += "8084/state?id=5senior";
+			break;
+		case 16:
+			HTTPreq += "8083/state?id=5senior";
+			break;
+		case 32:
+			HTTPreq += "8082/twist?id=5senior&lx=";
+			HTTPreq += requestData;
+			break;
+		case 64:
+			HTTPreq += "8082/twist?id=5senior&az=";
+			HTTPreq += requestData;
+			break;
+		case 128:
+			HTTPreq += "8082/twist?id=5senior&lx=0";
+			break;
+	}
     
     close(sock);
     exit(0);
 }
 
+int generatePassword() {
+  return rand() % 20000000;
+}
+
 int main(int argc, char *argv[]) {
-	string hostName;
-	int robotID, robotNum;
-	in_port_t port;
-	char* UdpServerPort;
+ 
+  srand(time(NULL));
 
-	if (argc != 9) {
-		cout << "Usage: " << argv[0] << " -h <hostname-of-robot> -i <robot-id> -n " <<
-			"<robot-number> -p <port>" << endl;
-		exit(1);
-	}
+  if (argc != 9) {
+    cout << "Usage: " << argv[0] << " -h <hostname-of-robot> -i <robot-id> -n " 
+      << "<robot-number> -p <port>" << endl;
+    exit(1);
+  }
 
-	for (int i = 0; i < 9; i++) {
-		string argvStr = argv[i];
-		if (argvStr.compare("-h") == 0) {
-			hostName = argv[i+1];
-		} else if (argvStr.compare("-i") == 0) {
-			robotID = atoi(argv[i+1]);
-		} else if (argvStr.compare("-n") == 0) {
-			robotNum = atoi(argv[i+1]);
-		} else if (argvStr.compare("-p") == 0) {
-			port = atoi(argv[i+1]);
-			UdpServerPort = argv[i+1];
-		}
-	}
+  for (int i = 0; i < 9; i++) {
+    string argvStr = argv[i];
+    if (argvStr.compare("-h") == 0) {
+      hostName = argv[i+1];
+    } else if (argvStr.compare("-i") == 0) {
+      robotID = atoi(argv[i+1]);
+    } else if (argvStr.compare("-n") == 0) {
+      robotNum = atoi(argv[i+1]);
+    } else if (argvStr.compare("-p") == 0) {
+      port = atoi(argv[i+1]);
+      UdpServerPort = argv[i+1];
+    }
+  }
 
-	UdpServer(UdpServerPort);
+  UdpServer(UdpServerPort);
 }
